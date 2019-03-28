36886e52fad89d22e8e1666917e99cf8c5796e2e
JavaFXpert <james@j1w.xyz>
2019-03-28 17:10:40 -0400

    setup for passing in solution

--- viz/network_graph.py
+++ viz/network_graph.py
@@ -31,33 +31,39 @@ class NetworkGraph(pygame.sprite.Sprite):
         self.rect = None
         self.adj_matrix = None
         self.set_adj_matrix(adj_matrix)
+        self.solution = None
 
-    def set_adj_matrix(self, adj_matrix):
+    def set_adj_matrix(self, adj_matrix, solution=None):
         self.adj_matrix = adj_matrix
+        num_nodes = self.adj_matrix.shape[0]  # Number of nodes in graph
+
+        if solution:
+            self.solution = solution
+        else:
+            self.solution = np.zeros(num_nodes)
 
         fig = plt.figure(figsize=(5, 5))
         ax = plt.subplot(111)
 
-        num_nodes = self.adj_matrix.shape[0]  # Number of nodes in graph
-        G = nx.Graph()
-        G.add_nodes_from(np.arange(0, num_nodes, 1))
+        graph = nx.Graph()
+        graph.add_nodes_from(np.arange(0, num_nodes, 1))
 
+        # tuple is (i,j,weight) where (i,j) is the edge
         edge_list = []
         for i in range(num_nodes):
             for j in range(i + 1, num_nodes):
                 if adj_matrix[i, j] != 0:
                     edge_list.append((i, j, 1.0))
 
-        # elist = [(0, 1, 1.0), (0, 2, 1.0), (0, 3, 1.0), (1, 2, 1.0), (2, 3, 1.0)]
-        # tuple is (i,j,weight) where (i,j) is the edge
+        graph.add_weighted_edges_from(edge_list)
 
-        G.add_weighted_edges_from(edge_list)
-
-        colors = ['r' for node in G.nodes()]
-        pos = nx.spring_layout(G)
+        colors = ['r' for node in graph.nodes()]
+        pos = nx.spring_layout(graph)
         default_axes = plt.axes(frameon=True)
 
-        nx.draw_networkx(G, node_color=colors, node_size=600, alpha=.8, ax=default_axes, pos=pos)
+        colors = ['r' if self.solution[i] == 0 else 'b' for i in range(num_nodes)]
+
+        nx.draw_networkx(graph, node_color=colors, node_size=600, alpha=.8, ax=default_axes, pos=pos)
         plt.savefig("utils/data/network_graph.png")
 
         self.image, self.rect = load_image('network_graph.png', -1)
