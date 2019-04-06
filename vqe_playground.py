e6e9846c4e2e37d6aea1bbebca6044256376bede
JavaFXpert <james@j1w.xyz>
2019-04-06 19:02:25 -0400

    Add another adjacency matrix and modify order in which rotation gates are processed

--- vqe_playground.py
+++ vqe_playground.py
@@ -159,9 +159,9 @@ class VQEPlayground():
         initial_adj_matrix = np.array([
             [0, 2, 1, 1, 4],
             [2, 0, 3, 3, 1],
-            [1, 3, 0, 2, 0],
+            [1, 3, 0, 2, 1],
             [1, 3, 2, 0, 1],
-            [4, 1, 0, 1, 0]
+            [4, 1, 1, 1, 0]
         ])
 
         # maxcut_op, maxcut_shift = maxcut.get_maxcut_qubitops(initial_adj_matrix)
